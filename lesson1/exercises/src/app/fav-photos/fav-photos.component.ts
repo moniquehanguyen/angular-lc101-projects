5fea9e7b1d58dc4418424e5f899bdcaa91b86bcd
Monique Nguyen <moniquehanguyen@gmail.com>
2020-09-10 19:32:13 -0500

    part 1 of exercise completed.

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://chuckanddons.com/media/wysiwyg/kitten_blog.jpg';
+  image3 = 'https://i2.wp.com/news.minneapolismn.gov/wp-content/uploads/2018/08/KittenFluffs4.jpg';
 
   constructor() { }
 
